5e6d6aa394165845d57e199579110cb459a81101
Mohammad Rahmani <rto1680@gmail.com>
2019-10-02 21:22:17 +0430

    Make main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,168 @@
+import 'package:day6_fitness/Dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  PageController _pageController;
+
+  AnimationController rippleController;
+  AnimationController scaleController;
+
+  Animation<double> rippleAnimation;
+  Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController(
+      initialPage: 0
+    );
+
+    rippleController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1)
+    );
+
+    scaleController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1)
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: Dashboard()));
+      }
+    });
+
+    rippleAnimation = Tween<double>(
+      begin: 80.0,
+      end: 90.0
+    ).animate(rippleController)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        rippleController.reverse();
+      } else if(status == AnimationStatus.dismissed) {
+        rippleController.forward();
+      }
+    });
+
+    scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 30.0
+    ).animate(scaleController);
+
+    rippleController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          makePage(image: 'assets/images/one.jpg'),
+          makePage(image: 'assets/images/two.jpg'),
+          makePage(image: 'assets/images/three.jpg'),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  Widget makePage({image}) {
+    return Container(
+      decoration: BoxDecoration(
+        // image: DecorationImage(
+        //   image: AssetImage(image),
+        //   fit: BoxFit.cover
+        // )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.black.withOpacity(.3),
+              Colors.black.withOpacity(.3),
+            ]
+          )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 60,),
+              Text('Exerciese 1', style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+              SizedBox(height: 40,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("15", style: TextStyle(color: Colors.yellow[400], fontSize: 40, fontWeight: FontWeight.bold),),
+                  Text("Minutes", style: TextStyle(color: Colors.white, fontSize: 30),),
+                ],
+              ),
+              SizedBox(height: 30,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("3", style: TextStyle(color: Colors.yellow[400], fontSize: 40, fontWeight: FontWeight.bold),),
+                  Text("Exercises", style: TextStyle(color: Colors.white, fontSize: 30),),
+                ],
+              ),
+              SizedBox(height: 180,),
+              Align(
+                child: Text("Start the morning with your health", 
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w100),),
+              ),
+              SizedBox(height: 30,),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: AnimatedBuilder(
+                  animation: rippleAnimation,
+                  builder: (context, child) => Container(
+                    width: rippleAnimation.value,
+                    height: rippleAnimation.value,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue.withOpacity(.4)
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          scaleController.forward();
+                        },
+                        child: AnimatedBuilder(
+                          animation: scaleAnimation,
+                          builder: (context, child) => Transform.scale(
+                            scale: scaleAnimation.value,
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
